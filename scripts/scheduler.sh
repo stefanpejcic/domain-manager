4e8df45538169d1edb8337f48954159cd315dd03
Stefan Pejcic <stefan@pejcic.rs>
2024-12-18 16:02:17 +0100

    Update scheduler.sh

--- scripts/scheduler.sh
+++ scripts/scheduler.sh
@@ -3,7 +3,9 @@
 # First argument is the check type
 CHECK_TYPE=$1
 
-USERS_DIR="users/"
+SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+USERS_DIR="${SCRIPTS_DIR}/../users/"
+
 
 # Iterate over each JSON file in the users directory
 for user_file in $USERS_DIR*.json; do
@@ -13,19 +15,19 @@ for user_file in $USERS_DIR*.json; do
       case $CHECK_TYPE in
         --all)
           echo "$domain"
-          bash /root/domain-manager/scripts/http_response_check.sh $domain
-          bash /root/domain-manager/scripts/ssl_check.sh $domain
-          bash /root/domain-manager/scripts/whois_check.sh $domain
+          bash ${SCRIPTS_DIR}/http_response_check.sh $domain
+          bash ${SCRIPTS_DIR}/ssl_check.sh $domain
+          bash ${SCRIPTS_DIR}/whois_check.sh $domain
           echo "____________________________________________________________"
           ;;
         http_response_check)
-          bash /root/domain-manager/scripts/http_response_check.sh $domain
+          bash ${SCRIPTS_DIR}/http_response_check.sh $domain
           ;;
         ssl_check)
-          bash /root/domain-manager/scripts/ssl_check.sh $domain
+          bash ${SCRIPTS_DIR}/ssl_check.sh $domain
           ;;
         whois_check)
-          bash /root/domain-manager/scripts/whois_check.sh $domain
+          bash ${SCRIPTS_DIR}/whois_check.sh $domain
           ;;
 
         *)
